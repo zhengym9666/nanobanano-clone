dee5a0fdc7bf09e10807bc62b77cd254281fd0a9
zym <1039230702@qq.com>
2026-01-17 12:09:38 +0800

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1938157335264292"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className={`font-sans antialiased`} suppressHydrationWarning={true}>
         <AuthProvider>
           {children}
