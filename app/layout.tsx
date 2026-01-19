c133c33ba6b173d942c4691554e2253d114d42cb
zym <1039230702@qq.com>
2026-01-19 21:55:23 +0800

    ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5F2XMBS6J7"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5F2XMBS6J7');
+        </script>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1938157335264292"
